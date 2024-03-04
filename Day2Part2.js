b4acb70b6c56feeef071c693aa1a751ed63f8f34
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-04 19:46:11 +0500

    Add files via upload

---
+++ Day2Part2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Andrew Tate";
+let message = `Once ${famous_person} said,\"Do The Impossible And You'll Never Doubt Yourself Again\"`;
+console.log(message);
---
+++ Day2Part2.ts
@@ -0,0 +1,3 @@
+let famous_person: string="Andrew Tate"
+let message :string=`Once ${famous_person} said,\"Do The Impossible And You'll Never Doubt Yourself Again\"`
+console.log(message)
