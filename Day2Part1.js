f0b0fb07916917dd7e9ed141849ddd599a3d0f47
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-04 19:34:27 +0500

    Update Day2Part1.js

--- Day2Part1.js
+++ Day2Part1.js
@@ -1,3 +1,3 @@
 "use strict";
-let quote = "Once Andrew Tate said,\"Do The Impossible And You'll Never Doubt Yourself Again";
+let quote = "Once Andrew Tate said,\"Do The Impossible And You'll Never Doubt Yourself Again\"";
 console.log(quote);
