f4978d58d511861a74246e7b68d6cba1d70f0653
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-04 20:20:08 +0500

    Add files via upload

---
+++ Day2Part3.js
@@ -0,0 +1,4 @@
+"use strict";
+let Person_Name = "\n\tMuhammad Hussain Ullah\t\n"; //\nfor new line and \t for horizontal space
+console.log(Person_Name);
+console.log(Person_Name.trim());
---
+++ Day2Part3.ts
@@ -0,0 +1,3 @@
+let Person_Name : string= "\n\tMuhammad Hussain Ullah\t\n"; //\nfor new line and \t for horizontal space
+console.log(Person_Name);
+console.log(Person_Name.trim())
