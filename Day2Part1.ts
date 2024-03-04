403c96a8b007164d87d033011bfe4e97095e09c0
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-04 19:34:04 +0500

    Update Day2Part1.ts

--- Day2Part1.ts
+++ Day2Part1.ts
@@ -1,2 +1,2 @@
-let quote :string= "Once Andrew Tate said,\"Do The Impossible And You'll Never Doubt Yourself Again"
+let quote :string= "Once Andrew Tate said,\"Do The Impossible And You'll Never Doubt Yourself Again\""
 console.log(quote)
