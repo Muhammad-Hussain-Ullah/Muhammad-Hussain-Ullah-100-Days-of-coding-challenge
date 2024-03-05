2e4f1a3929ef09516e6cd767ad783b52cea5e852
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-05 10:17:15 +0500

    Add files via upload

---
+++ Day3Part3.js
@@ -0,0 +1,3 @@
+"use strict";
+let favourite_number = 9642;
+console.log(`My Favourite number is ${favourite_number}`);
---
+++ Day3Part3.ts
@@ -0,0 +1,2 @@
+let favourite_number : number= 9642
+console.log(`My Favourite number is ${favourite_number}`)
