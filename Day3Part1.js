39cbae61378c799d2c4abb3bfd114b073203ded9
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-05 10:04:04 +0500

    Add files via upload

---
+++ Day3Part1.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(7 + 1); //this will add the numbers and print the output
+console.log(9 - 1); //this will subtract the number and print the output
+console.log(4 * 2); //this will multiply the number and print the output
+console.log(16 / 2); //this will divide the number and print the output 
