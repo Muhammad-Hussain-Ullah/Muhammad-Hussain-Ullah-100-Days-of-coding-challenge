2361e9d78c187e90269e696d659ab80260266de6
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-05 10:13:42 +0500

    Add files via upload

---
+++ Day3Part2.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 4, b = 4, c = 5, d = 3, e = 2, f = 6, g = 1;
+console.log(a + b);
+console.log(d + c);
+console.log(e + f);
+console.log(c + e + g);
---
+++ Day3Part2.ts
@@ -0,0 +1,5 @@
+let a : number= 4, b : number= 4,c : number=5,d:number=3,e:number=2,f:number=6,g:number=1;
+console.log(a+b)
+console.log(d+c)
+console.log(e+f)
+console.log(c+e+g)
