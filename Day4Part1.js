20c678de2766d3909abbf87ad8356730caf84163
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-05 10:22:52 +0500

    Add files via upload

---
+++ Day4Part1.js
@@ -0,0 +1,7 @@
+"use strict";
+//My name is huusain
+//Date is 3//5/2024
+//this program does the baisc maths operation of addition
+let my_number = 9600;
+let my_number2 = 42;
+console.log(`My favourite number is ${my_number + my_number2}`);
---
+++ Day4Part1.ts
@@ -0,0 +1,6 @@
+//My name is huusain
+//Date is 3//5/2024
+//this program does the baisc maths operation of addition
+let my_number : number = 9600
+let my_number2 : number= 42
+console.log(`My favourite number is ${my_number+my_number2}`)
