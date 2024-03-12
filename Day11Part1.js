156ef2eea0646510a417d3d849a3d61f50673658
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-03-12 09:19:17 +0500

    Add files via upload

---
+++ Day11Part1.js
@@ -0,0 +1,8 @@
+"use strict";
+let user_names = [];
+if (user_names.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Greet users
+}
---
+++ Day11Part1.ts
@@ -0,0 +1,7 @@
+let user_names: string[] = [];
+
+if (user_names.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    // Greet users
+}
